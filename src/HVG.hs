ffd479a238cb74fc0e5b679e561ccf46fda0b0fa
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2016-04-02 01:37:21 +0800

    query not found hint

--- src/HVG.hs
+++ src/HVG.hs
@@ -2,6 +2,9 @@ module HVG
     ( drawCanvas
     ) where
 
+import Control.Monad
+import qualified Data.Map.Strict as M
+
 import HVG.Type
 import HVG.Context2D
 
@@ -12,7 +15,16 @@ drawCanvas canvasCSSQuery size (Builder drawBuilder) = do
   putStrLn $ "  var canvas = document.querySelector(" ++ show canvasCSSQuery ++ ")";
   putStrLn $ "  if( !canvas ) return;"
   putStrLn $ "  var ctx = canvas.getContext('2d');"
-  let BuilderPartDone _ bld _ = drawBuilder (initContextState size) initBuilderState
-  bldDraw bld
+  case drawBuilder (initContextState size) initBuilderState of
+    BuilderPartDone _ bld _ -> do
+      forM_ (M.toList (bldWaitDraw bld)) $ \(drawName, _) ->
+        putStrLn $ "  console.warn('wait no draw: ' + " ++ show drawName ++ ")"
+      forM_ (M.toList (bldWaitLink bld)) $ \(linkName, _) ->
+        putStrLn $ "  console.warn('wait no link: ' + " ++ show linkName ++ ")"
+      bldDraw bld
+    BuilderPartWaitDraw drawName _ _ ->
+      putStrLn $ "  console.warn('wait no link: ' + " ++ show drawName ++ ")"
+    BuilderPartWaitLink linkName _ _ ->
+      putStrLn $ "  console.warn('wait no draw: ' + " ++ show linkName ++ ")"
   putStrLn   "})();"
 
